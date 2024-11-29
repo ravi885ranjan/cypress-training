5612b13c79642e30b36791630e57182df6c8ecc3
ravi8 <ravi885ranjan@gmail.com>
2024-11-29 10:50:16 +0000

    HomePage page object

---
+++ cypress/support/pageObject/amazon/HomePage.js
@@ -0,0 +1,26 @@
+class HomePage {
+    searchByProductByText(searchText){
+    cy.get('#twotabsearchtextbox').type(searchText)
+    cy.get('#nav-search-submit-button').click()
+    }
+
+    sortByPrice(){
+    cy.contains('Sort by:').click({force:true})
+    cy.get('[aria-labelledby="s-result-sort-select_1"]').trigger('click')
+    cy.wait(2000)
+    }
+
+    validateItemsPriceInAscOrder(){
+        const pricesInOrder = [];
+        cy.get('.a-price span.a-offscreen').each(($e1, index, $list)=>{
+            cy.wrap($e1).invoke('prop', 'innerText').then(function(text){
+                pricesInOrder[index] = Number(String(text).replace('$',''))
+            })
+        }).then(function(){
+            for(let i=0;i<pricesInOrder.length;i++){
+                expect(pricesInOrder[i]).to.be.lte(pricesInOrder[i+1])
+            }
+        })
+    }
+}
+export default HomePage;
